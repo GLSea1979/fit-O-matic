566b97e5dbd015b46d72ad2f736b410a8fb68983
reganoneill <rkrotho@gmail.com>
2017-03-13 15:05:45 -0700

    more user routes

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -23,3 +23,30 @@ authRouter.post('/api/signup', jsonParser, function(req, res, next){
   .then( token => res.send(token))
   .catch(next);
 });
+
+authRouter.get('/api/signin', basicAuth, function(req, res, next){
+  debug('GET: /api/signin');
+
+  User.findOne({username: req.auth.username})
+  .then( user => user.comparePasswordHash(req.auth.password))
+  .then( user => user.generateToken())
+  .then( token => res.send(token))
+  .catch(next);
+});
+
+authRouter.put('/api/newPassword', basicAuth, jsonParser, function(req, res, next){
+  debug('PUT: /api/newPassword');
+  req.body.password;
+  delete req.body.password;
+
+  User.findOne({username: req.auth.username})
+  .then( user => user.comparePasswordHash(req.auth.password))
+  .then( user => User.findByIdAndUpdate(user._id, user, {$new: true} ))
+  .then( user => {
+    if(!user){
+      return next(createError(404, 'user not found'))
+    }
+    res.send('password updated');
+  })
+  .catch(next);
+});
