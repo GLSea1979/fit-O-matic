7e3dfc1c871c937d2dcfa43bbfa66eaf74fde12d
reganoneill <rkrotho@gmail.com>
2017-03-14 23:42:50 -0700

    profile routes built mostly tested

--- routes/profile-route.js
+++ routes/profile-route.js
@@ -5,7 +5,7 @@ const jsonParser = require('body-parser').json();
 const Router = require('express').Router;
 const createError = require('http-errors');
 
-const User = require('../model/user.js');
+// const User = require('../model/user.js');
 const Profile = require('../model/profile.js');
 const bearerAuth = require('../lib/bearer-auth-middleware.js');
 
@@ -15,11 +15,32 @@ profileRouter.get('/api/profile/:id', bearerAuth ,function(req, res, next){
   debug('GET: /api/profile/:id');
   Profile.findOne({userID:req.params.id})
   .then( profile => {
-    debug(profile);
-
+    
     if(!profile) return next(createError(404, 'biffed it'));
 
     res.json(profile);
   })
+  .catch(next);
+});
+
+profileRouter.put('/api/profile/:id', bearerAuth, jsonParser, function(req, res, next){
+  debug('PUT: /api/profile/:id');
+  if(!req.body) return next(createError(400, 'bad request'));
+  Profile.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then( profile => {
+    res.json(profile);
+  })
+  // .catch(next);
+  .catch(err => {
+    debug('here is our error:', err);
+    next();
+  });
+
+});
+
+profileRouter.delete('/api/profile/:id', bearerAuth, function(req, res, next){
+  debug('DELETE: /api/profile/:id');
+  Profile.findByIdAndRemove(req.params.id)
+  .then( () => res.sendStatus(204))
   .catch(next);
 });
--- test/profile-test.js
+++ test/profile-test.js
@@ -3,10 +3,13 @@
 const request = require('superagent');
 const expect = require('chai').expect;
 const Promise = require('bluebird');
-const debug = require('debug')('fit-O-matic:profile-test');
+const mongoose = require('mongoose');
+const debug = require('debug')('fit-O-matic:profile-route-test');
 
 const User = require('../model/user.js');
 const Profile = require('../model/profile.js');
+
+mongoose.Promise = Promise;
 
 const url = `http://localhost:${process.env.PORT}`;
 
@@ -23,8 +26,13 @@ const sampleProfile = {
   gender: 'man',
   photo: 'sweetphoto.png'
 };
+const sampleUpdatedProfile = {
+  name: 'Mr. Updated Name Property!!!',
+  gender: 'woman',
+  photo: 'doje.png'
+};
 
-describe('Profile Routes', function(){
+describe.only('Profile Routes', function(){
   afterEach( done => {
     Promise.all([
       User.remove({}),
@@ -34,6 +42,85 @@ describe('Profile Routes', function(){
     .catch(done);
   });
   describe('GET: /api/profile/:id', function(){
+    describe('with a valid ID', () => {
+      before( done => {
+        new User(sampleUser)
+        .generatePasswordHash(sampleUser.password)
+        .then( user => user.save())
+        .then( user => {
+          this.tempUser = user;
+          return user.generateToken();
+        })
+        .then( token => {
+          this.tempToken = token;
+          done();
+        })
+        .catch(done);
+      });
+
+      before( done => {
+        sampleProfile.userID = this.tempUser._id;
+        new Profile(sampleProfile).save()
+        .then( profile => {
+          this.tempProfile = profile;
+          done();
+        })
+        .catch(done);
+      });
+      it('should return a valid profile', done => {
+        request.get(`${url}/api/profile/${this.tempUser._id}`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((err, res) => {
+          if(err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('tested');
+          expect(res.body.userID).to.equal(this.tempUser._id.toString());
+          done();
+        });
+      });
+    });
+
+    describe('with an invalid ID', () => {
+      before( done => {
+        new User(sampleUser)
+        .generatePasswordHash(sampleUser.password)
+        .then( user => user.save())
+        .then( user => {
+          this.tempUser = user;
+          return user.generateToken();
+        })
+        .then( token => {
+          this.tempToken = token;
+          done();
+        })
+        .catch(done);
+      });
+
+      before( done => {
+        sampleProfile.userID = this.tempUser._id;
+        new Profile(sampleProfile).save()
+        .then( profile => {
+          this.tempProfile = profile;
+          done();
+        })
+        .catch(done);
+      });
+      it('should return a 400 for CastError', done => {
+        request.get(`${url}/api/profile/:ohnooo`)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        done();
+      });
+      });
+    });//end GET
+  });
+
+  describe('PUT: /api/profile/:id', function(){
     before( done => {
       new User(sampleUser)
       .generatePasswordHash(sampleUser.password)
@@ -47,7 +134,7 @@ describe('Profile Routes', function(){
         done();
       })
       .catch(done);
-    });
+    });//end before
 
     before( done => {
       sampleProfile.userID = this.tempUser._id;
@@ -58,22 +145,63 @@ describe('Profile Routes', function(){
       })
       .catch(done);
     });
-    describe('with a valid ID', () => {
-      debug('**********************************************0293203920932093092039203992039203920392*********');
-      it('should return a profile', done => {
-        request.get(`${url}/api/profile/${this.tempUser._id}`)
+    describe('with a valid request body', () => {
+      it('should return an updated profile object', done => {
+        request.put(`${url}/api/profile/${this.tempProfile._id}`)
+        .send(sampleUpdatedProfile)
         .set({
           Authorization: `Bearer ${this.tempToken}`
         })
         .end((err, res) => {
-          debug('HERE IS THE res body', res.body);
-          if(err) return done(err);
+          if (err) return done(err);
+          debug('res.body ------->', res.body);
+          expect(res.body.name).to.equal(sampleUpdatedProfile.name);
+          expect(res.body.gender).to.equal(sampleUpdatedProfile.gender);
           expect(res.status).to.equal(200);
-          expect(res.body.name).to.equal('tested');
-          expect(res.body.userID).to.equal(this.tempUser._id.toString());
           done();
         });
       });
     });
   });
+
+  describe('DELETE: /api/profile/:id', function(){
+    before( done => {
+      new User(sampleUser)
+        .generatePasswordHash(sampleUser.password)
+        .then( user => user.save())
+        .then( user => {
+          this.tempUser = user;
+          return user.generateToken();
+        })
+        .then( token => {
+          this.tempToken = token;
+          done();
+        })
+        .catch(done);
+    });//end before
+
+    before( done => {
+      sampleProfile.userID = this.tempUser._id;
+      new Profile(sampleProfile).save()
+        .then( profile => {
+          this.tempProfile = profile;
+          done();
+        })
+        .catch(done);
+    });
+    describe('with a valid id', () => {
+      it('should return a 204 response for deleting a profile', done => {
+        request.delete(`${url}/api/profile/${this.tempProfile._id}`)
+          .set({
+            Authorization: `Bearer ${this.tempToken}`
+          })
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).to.equal(204);
+            done();
+          });
+      });
+    });
+  });
+
 });
