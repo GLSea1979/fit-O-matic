699445a86343f2d51b16a0225e3df6e74e5bae4a
Zachary Crumbo <zcrumbo@mac.com>
2017-04-15 17:26:51 -0700

    added additional geo by id route, updated tests, and updated algorithm to include range

--- test/geo-test.js
+++ test/geo-test.js
@@ -33,6 +33,11 @@ const sampleBikeGeo = {
   topTubeLength: 52,
   bikeID: []
 };
+const sampleBikeGeo2 = {
+  bikeSizeName: 'test size2',
+  topTubeLength: 50,
+  bikeID: []
+};
 const sampleUser = {
   username: 'test username',
   email: 'test email',
@@ -128,21 +133,89 @@ describe('Bike Geometry Routes', function() {
       });
     });
     describe('with an invalid bike ID', () => {
-      it('should return a 404', done => {
+      it('should return a 400', done => {
         request.post(`${url}/api/bike/${this.tempBike._id.toString().slice(0, -3)}ccc/geometry`)
         .send(this.tempBikeGeo)
         .set({
           Authorization: `Bearer ${this.tempToken}`
         })
         .end((err, res) => {
-          expect(err.message).to.equal('Not Found');
-          expect(res.status).to.equal(404);
+          expect(err.message).to.equal('Bad Request');
+          expect(res.status).to.equal(400);
           done();
         });
       });
     });  });
+  describe('GET: /api/geo/:geoID', function() {
 
-  describe('GET: /api/geo/:id', function() {
+    before( done => {
+      new Mfr(sampleMfr).save()
+      .then( mfr => {
+        this.tempMfr = mfr;
+      })
+      .catch(done);
+
+      let user = new User(sampleUser);
+      user.generatePasswordHash(sampleUser.password)
+      .then( user => user.save())
+      .then( user => {
+        this.tempUser = user;
+        return user.generateToken();
+      })
+      .then( token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(done);
+    });
+
+    before( done => {
+      sampleBikeProfile.mfrID = this.tempMfr._id;
+      new BikeProfile(sampleBikeProfile).save()
+      .then(bike => {
+        this.tempBike = bike;
+        done();
+      })
+      .catch(done);
+    });
+
+    before( done => {
+      sampleBikeGeo.bikeID.push(this.tempBike._id);
+      new BikeGeo(sampleBikeGeo).save()
+      .then( bikeGeo => {
+        this.tempBikeGeo = bikeGeo;
+        done();
+      })
+      .catch(done);
+    });
+
+    it('should return a bike geometry object', done => {
+      request.get(`${url}/api/geo/${this.tempBikeGeo._id}`)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        debug(res.body, 'first one by ID');
+        expect(res.status).to.equal(200);
+        expect(res.body.bikeSizeName).to.equal(this.tempBikeGeo.bikeSizeName);
+        expect(res.body.topTubeLength).to.equal(sampleMeasure.result);
+        done();
+      });
+    });
+    it('should return a 400 with an invalid id', done => {
+      request.get(`${url}/api/geo/58f2b7eeeaace9dbc827247f`)
+      .set({
+        Authorization: `Bearer ${this.tempToken}`
+      })
+      .end((err, res) => {
+        expect(err.message).to.equal('Bad Request');
+        expect(res.status).to.equal(400);
+        done();
+      });
+    });
+  });
+  describe('GET: /api/geo/?querystring', function() {
 
     before( done => {
       new Mfr(sampleMfr).save()
@@ -183,10 +256,16 @@ describe('Bike Geometry Routes', function() {
     before( done => {
       sampleBikeGeo.bikeID.push(this.tempBike._id);
       sampleBikeGeo.bikeID.push(this.tempBike2._id);
+      sampleBikeGeo2.bikeID.push(this.tempBike._id);
+      sampleBikeGeo2.bikeID.push(this.tempBike2._id);
       new BikeGeo(sampleBikeGeo).save()
       .then( bikeGeo => {
         this.tempBikeGeo = bikeGeo;
-        done();
+        new BikeGeo(sampleBikeGeo2).save()
+        .then( bikeGeo => {
+          this.tempBikeGeo2 = bikeGeo;
+          done();
+        });
       })
       .catch(done);
     });
