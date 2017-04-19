bf2a9f83e38c4455e70963f6e280e9f55095b629
reganoneill <rkrotho@gmail.com>
2017-04-19 14:17:40 -0700

    populate mfrIDs

--- model/bike.js
+++ model/bike.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 
 const BikeSchema = Schema({
   bikeName: { type: String, required: true },
-  mfrID: { type: Schema.Types.ObjectId, required: true},
+  mfrID: { type: Schema.Types.ObjectId, required: true, ref: 'mfr'},
   category: { type: String, required: true},
   photoURI: { type: String, required: true},
   photoKey: { type: String, required: false},
